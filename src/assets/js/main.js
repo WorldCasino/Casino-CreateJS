85def6b30a59eb21ac90fb1451caef42d995302c
Zhabko Dmitry <dmitry.zhabko@gmail.com>
2016-02-12 17:48:56 +0200

    5 rows with spin;

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -13,13 +13,93 @@ function init() {
 	var elementsMas = [],
 			elementsPositions = [];
 
-	for (var i = 0; i < elementNumberAll; i++ ) {
-		elementsMas[i] = Math.floor(Math.random() * 60);
-		elementsPositions[i] = -elementHeight * ( elementNumberAll - elementNumberOnScreen - i );
+	var rows = [],
+	 		currentScreen = [];
+
+	function getFirstRow() {
+		var row = new createjs.Container();
+
+		for (var i = 0; i < elementNumberOnScreen; i++ ) {
+			elementsMas[i] = Math.floor(Math.random() * 60);
+			elementsPositions[i] = elementHeight * i;
+
+			var text = new createjs.Text(elementsMas[i], elementHeight*0.6 +"px Arial", "#222222");
+			text.lineHeight = elementHeight;
+			text.textBaseline = "top";
+			text.x = 10;
+			text.y = elementsPositions[i];
+
+			row.addChild(text);
+		}
+		return row;
 	}
 
-	var text = new createjs.Text(elementsMas[0], "40px Arial", "#222222");
+	function getFirstScreen() {
+		for (var i = 0; i < rowNumber; i++) {
+			rows[i] = getFirstRow();
+			currentScreen[i] = rows[i].children.slice();
+			rows[i].x = rowWidth*i;
+			stage.addChild(rows[i]);
+		}
+		stage.update();
+	}
 
-	console.log(elementsPositions);
+	function getNewRow() {
+		var row = new createjs.Container();
+
+		for (var i = 0; i < elementNumberAll; i++ ) {
+			elementsMas[i] = Math.floor(Math.random() * 60);
+			elementsPositions[i] = -elementHeight * ( elementNumberAll - elementNumberOnScreen - i );
+
+			var text = new createjs.Text(elementsMas[i], elementHeight*0.6 +"px Arial", "#222222");
+			text.lineHeight = elementHeight;
+			text.textBaseline = "top";
+			text.x = 10;
+			text.y = elementsPositions[i];
+
+			row.addChild(text);
+		}
+
+		return row;
+	}
+
+	function getNewSpin() {
+		for (var i = 0; i < rowNumber; i++) {
+			stage.removeChild(rows[i])
+			rows[i] = getNewRow();
+
+// --    Здесь происходит замена новых элементов
+//---       на элементы с последнего экрана и
+//---          установка их расположения
+			for (var j = 0; j < elementNumberOnScreen; j++) {
+				currentScreen[i][j].y = elementHeight * j;
+			}
+			rows[i].children.splice(57, 3);
+			Array.prototype.push.apply(rows[i].children, currentScreen[i]);
+			currentScreen[i] = rows[i].children.slice(0, 3);
+
+
+			rows[i].x = rowWidth*i;
+			stage.addChild(rows[i]);
+		}
+		stage.update();
+	}
+
+	getFirstScreen();
+
+	var spinButton = document.getElementById('spinButton');
+	spinButton.addEventListener("click", function() {
+		getNewSpin();
+
+		for (var i = 0; i < rows.length; i++) {
+			var time = 1500*3 + Math.random()*500*3;
+			createjs.Tween.get(rows[i], {loop: false})
+				.to({ y: -elementsPositions[0] }, time , createjs.Ease.getPowInOut(2.2));
+		}
+
+	});
+
+	createjs.Ticker.setFPS(60);
+	createjs.Ticker.addEventListener("tick", stage);
 
 }
