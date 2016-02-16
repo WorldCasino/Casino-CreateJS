4d7226dde2fa03046f6be93de72e0918168baa93
Zhabko Dmitry <dmitry.zhabko@gmail.com>
2016-02-16 18:40:11 +0200

    responsive game

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,34 @@
 var stage;
 function init() {
-	stage = new createjs.Stage("canvas");
+	var canvas = document.getElementById("canvas");
+	canvas.style.width = document.documentElement.clientWidth*0.8 + "px";
+	canvas.style.height = parseInt(canvas.style.width)*0.5625 + "px";
 
+	if(parseInt(canvas.style.width) > 1280){canvas.style.width = "1280px"}
+	var canvasWidth = parseInt(canvas.style.width),
+			canvasHeight = parseInt(canvas.style.height);
+
+	window.onresize = function() {
+		canvas.style.width = document.documentElement.clientWidth*0.8 + "px";
+		if(parseInt(canvas.style.width) > 1280){canvas.style.width = "1280px"}
+		canvas.style.height = parseInt(canvas.style.width)*0.5625 + "px";
+		canvasWidth = parseInt(canvas.style.width),
+		canvasHeight = parseInt(canvas.style.height);
+	}
+
+
+
+
+	stage = new createjs.Stage("canvas");
+	stage.canvas.width = canvasWidth;
+	stage.canvas.height = canvasHeight;
 	var rowNumber = 5,
-			rowWidth = stage.canvas.width/rowNumber;
+			rowWidth = canvasWidth/rowNumber;
 
 	var elementNumberAll = 60,
 			elementNumberOnScreen = 3,
 			elementWidth = rowWidth,
-			elementHeight = stage.canvas.height/elementNumberOnScreen;
+			elementHeight = canvasHeight/elementNumberOnScreen;
 
 	var elementsMas = [],
 			elementsPositions = [];
@@ -16,20 +36,24 @@ function init() {
 	var rows = [],
 	 		currentScreen = [];
 
+	var preload = new createjs.LoadQueue();
+			preload.addEventListener("fileload", function(){
+				stage.update();
+			});
+			for(var i = 1; i <= 10; i++) {
+				preload.loadFile("img/game/" + i + ".png");
+			}
+
 	function getFirstRow() {
 		var row = new createjs.Container();
 
 		for (var i = 0; i < elementNumberOnScreen; i++ ) {
-			elementsMas[i] = Math.floor(Math.random() * 60);
+			elementsMas[i] = Math.ceil(Math.random() * 10);
 			elementsPositions[i] = elementHeight * i;
+			var img = new createjs.Bitmap("img/game/" + elementsMas[i] + ".png");
+			img.y = elementsPositions[i];
 
-			var text = new createjs.Text(elementsMas[i], elementHeight*0.6 +"px Arial", "#222222");
-			text.lineHeight = elementHeight;
-			text.textBaseline = "top";
-			text.x = 10;
-			text.y = elementsPositions[i];
-
-			row.addChild(text);
+			row.addChild(img);
 		}
 		return row;
 	}
@@ -48,16 +72,15 @@ function init() {
 		var row = new createjs.Container();
 
 		for (var i = 0; i < elementNumberAll; i++ ) {
-			elementsMas[i] = Math.floor(Math.random() * 60);
+			elementsMas[i] = Math.ceil(Math.random() * 10);
 			elementsPositions[i] = -elementHeight * ( elementNumberAll - elementNumberOnScreen - i );
 
-			var text = new createjs.Text(elementsMas[i], elementHeight*0.6 +"px Arial", "#222222");
-			text.lineHeight = elementHeight;
-			text.textBaseline = "top";
-			text.x = 10;
-			text.y = elementsPositions[i];
+			var img = new createjs.Bitmap("img/game/" + elementsMas[i] + ".png");
+			img.y = elementsPositions[i];
+			img.width = elementWidth;
+			img.height = elementHeight;
 
-			row.addChild(text);
+			row.addChild(img);
 		}
 
 		return row;
@@ -90,10 +113,10 @@ function init() {
 	var spinButton = document.getElementById('spinButton');
 	spinButton.addEventListener("click", function() {
 		getNewSpin();
-
+		var spins = [];
 		for (var i = 0; i < rows.length; i++) {
-			var time = 1500*3 + Math.random()*500*3;
-			createjs.Tween.get(rows[i], {loop: false})
+			var time = 1500*2 + Math.random()*500*2;
+			spins[i] = createjs.Tween.get(rows[i], {loop: false})
 				.to({ y: -elementsPositions[0] }, time , createjs.Ease.getPowInOut(2.2));
 		}
 
@@ -103,3 +126,13 @@ function init() {
 	createjs.Ticker.addEventListener("tick", stage);
 
 }
+
+$.ajax({
+    url: 'http://192.168.0.250/JSON/SlotService.svc/Hello',
+    dataType: 'JSONP',
+    jsonpCallback: 'callback',
+    type: 'GET',
+    success: function (data) {
+        console.log(data);
+    }
+});
